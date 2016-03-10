31370bc2973a021abd4bb66fcc996736f2800416
Tore Norderud <tore.norderud@gmail.com>
2016-03-10 18:14:00 +0100

    Balanced Brackets: Legg til løsning

---
+++ balanced_brackets/torenord.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from sys import stdin
+
+def check(line):
+    stack = []
+
+    lparens = ["(", "[", "{"]
+    rparens = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+    }
+
+    for c in line:
+        if c in lparens:
+            stack.append(c)
+        if c in rparens:
+            if len(stack) == 0 or stack.pop() != rparens[c]:
+                return False
+
+    return True
+
+def main():
+    print(sum(check(line.strip()) for line in stdin))
+
+if __name__ == '__main__':
+    main()
