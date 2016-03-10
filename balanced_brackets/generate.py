4f3d00925be419660f85808ca28527780e8dd86a
Tore Norderud <tore.norderud@gmail.com>
2016-03-10 17:39:54 +0100

    Balanced Brackets/Parens: La til generate.py

---
+++ balanced_brackets/generate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from random import choice, randint
+
+def generate(depth):
+    """Print random looking balanced expression of specified depth"""
+
+    typ = choice(["()", "[]", "{}"])
+
+    if depth == 0:
+        return ""
+    if depth == 1:
+        return typ
+
+    return choice([lambda _: typ[0] + generate(depth-1) + typ[1],
+                   lambda r: generate(r) + generate(depth-r)])(randint(0, depth))
+
+def main():
+    for _ in range(50):
+        print generate(40)
+
+if __name__ == '__main__':
+    main()
