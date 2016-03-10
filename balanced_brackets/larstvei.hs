377d6e28333a06611aace680f61cc899cb744f89
Lars Tveito <larstvei@ifi.uio.no>
2016-03-10 23:55:52 +0100

    Forenklet Haskell-løsning av Balanced Brackets

--- balanced_brackets/larstvei.hs
+++ balanced_brackets/larstvei.hs
@@ -1,19 +1,8 @@
-dual ')' = '('
-dual ']' = '['
-dual '}' = '{'
-
-isOpen  = flip elem "([{"
-isClose = flip elem ")]}"
-
-closes x y = isClose x && dual x == y
-
-isBalanced' []     ys     = null ys
-isBalanced' (x:xs) []     = isBalanced' xs [x]
-isBalanced' (x:xs) (y:ys) | x `closes` y = isBalanced' xs ys
-                          | isOpen x     = isBalanced' xs (x:y:ys)
-                          | otherwise    = False
-
-isBalanced xs = isBalanced' xs []
+isBalanced xs = null $ foldl f [] xs
+    where f ('(':xs) ')' = xs
+          f ('[':xs) ']' = xs
+          f ('{':xs) '}' = xs
+          f xs        y  = y:xs
 
 main = do
   content <- getContents
