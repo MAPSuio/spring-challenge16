e445a1719d1fdc8b7b94eda33dbec43763566c83
Carl Martin Rosenberg <github@cmrosenberg.com>
2016-03-10 15:22:43 +0100

    Enter the puzzle dimension: Updated my magic solution

--- puzzle_dimension/carlmro.py
+++ puzzle_dimension/carlmro.py
@@ -1,8 +1,21 @@
-x = 228 / 4
-y = 228 / 4
+edge_bricks = 230
+total_bricks = 3240
 
-while x*y != 3168:
-    x += 1
-    y -= 1
+if (edge_bricks+4) % 4 == 0:
+    x = (edge_bricks+4) / 4
+    y = (edge_bricks+4) / 4
 
-print y, x
+    while x*y != total_bricks:
+        x += 1
+        y -= 1
+
+    print y, x
+else:
+    x = (edge_bricks+4) / 4
+    y = (edge_bricks+4) / 4 + 1
+
+    while x*y != total_bricks:
+        x += 1
+        y -= 1
+
+    print y, x
