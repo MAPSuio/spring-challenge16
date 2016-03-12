339e010179a484cd2404dd99872d1ad7629b59d2
Carl Martin Rosenberg <github@cmrosenberg.com>
2016-03-12 23:37:18 +0100

    Adding script for creating database insertions

---
+++ generate_db_insertions.py
@@ -0,0 +1,40 @@
+from os import path
+from os import walk
+
+CONTEST_START = "new Date"
+
+def create_entry(root, files):
+
+    answer_fd = open(path.join(root, 'answer.md'), 'ro')
+    answer = answer_fd.readlines()[0].strip()
+    answer_fd.close()
+
+    description_fd = open(path.join(root, 'README.md'), 'ro')
+    description = description_fd.readlines()
+    description_fd.close()
+
+    title = description[0].strip()[2:]
+
+    print "    Problems.insert"
+    print "      title: " + "\"" + title + "\""
+    print "      solution: " + "\"" + answer + "\""
+    print "      answers: []"
+    print "      draft: false"
+    print "      activeFrom: " + CONTEST_START
+    print "      description:" + "\"\"\"" + "".join(description[1:]) + "\"\"\""
+
+
+print "Meteor.startup ->"
+
+for root, _, files in walk('.'):
+
+    if root == ".":
+        continue
+
+    if root.startswith('./.git'):
+        continue
+
+    create_entry(root, files)
+
+print  "if Meteor.users.find().count() is 1\n Meteor.users.update {}, {$set: {isAdmin: true}}"
+
