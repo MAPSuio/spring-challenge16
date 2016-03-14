6849a0c7d4c99c6fe5621b5698cf4236fc3237ae
Lars Tveito <larstvei@ifi.uio.no>
2016-03-15 00:34:45 +0100

    Fjerne dependency fra Haskell-løsning på Words, Words, Words
    Data.List.Split kommer ikke med base-installasjonen av Haskell, så vi
    får implementere `chunksOf' selv.

--- words_words_words/larstvei.hs
+++ words_words_words/larstvei.hs
@@ -1,5 +1,7 @@
-import Data.List.Split (chunksOf)
 import Data.Char (ord, chr)
+
+chunksOf _ [] = []
+chunksOf n xs = take n xs : (chunksOf n $ drop n xs)
 
 score = (+10) . sum . map ord
 word = chr . (`rem` 128) . sum
@@ -7,4 +9,4 @@ word = chr . (`rem` 128) . sum
 main = do
   contents <- getContents
   let scores = map (word . map score) $ chunksOf 1000 $ lines contents
-  print scores
+  putStrLn scores
